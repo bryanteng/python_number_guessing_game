8c3a9729ff5ce74e9b6d68d70a6b6be3ba166bfe
github username <bteng1226@gmail.com>
2018-11-18 12:08:00 -0500

    added animations and a customizable range

--- guessing_game.py
+++ guessing_game.py
@@ -1,12 +1,33 @@
 import random
-def start():
+import time
+import sys
+
+def start(num):
     user_input = 0
-    random_num = str(random.randint(1,6))
+    nums = range(1,int(num)+1)
+    animation = ''.join(str(num) for num in nums)
+
+    print("Picking a random number ")
+    for i in range(50):
+
+        time.sleep(0.1)
+        sys.stdout.write("\r" + animation[i % len(animation)])
+        sys.stdout.flush()
+
+    print "\nDone!"
+    sys.stdout.flush()
+
+    random_num = str(random.randint(1,int(num)))
     print(random_num)
     while user_input != "exit" or user_input != random_num:
-        user_input = str(input("guess a number : "))
+        user_input = raw_input("guess a number : ")
         if user_input == random_num:
-            print("you win i guess")
-            break
+            print("you win!")
+            break;
+        elif user_input == "exit":
+            print("cya!")
+            break;
+        else:
+            print("incorrect! guess again!")
 
-start()
+start(raw_input("Pick an upperbound of numbers to guess between! : 1 .. "))
