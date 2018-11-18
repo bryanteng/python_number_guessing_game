a46cc3091185135004d2822a3b7f19bbe4ac4578
github username <bteng1226@gmail.com>
2018-11-18 13:18:39 -0500

    added user validation and tries counter

--- guessing_game.py
+++ guessing_game.py
@@ -3,31 +3,35 @@ import time
 import sys
 
 def start(num):
+    while int(num) < 1:
+        num = raw_input("Please pick a number greater than 1: ")
     user_input = 0
-    nums = range(1,int(num)+1)
+    nums = range(int(num)+1)
     animation = ''.join(str(num) for num in nums)
+    tries = 0
 
     print("Picking a random number ")
     for i in range(50):
-
         time.sleep(0.1)
         sys.stdout.write("\r" + animation[i % len(animation)])
         sys.stdout.flush()
 
-    print "\nDone!"
+    print "\nRandom number selected!"
     sys.stdout.flush()
 
     random_num = str(random.randint(1,int(num)))
-    print(random_num)
+
     while user_input != "exit" or user_input != random_num:
-        user_input = raw_input("guess a number : ")
+        user_input = raw_input("guess a number or type exit to end the game : ").strip()
         if user_input == random_num:
-            print("you win!")
+            tries+=1
+            print("you win! It took you " + str(tries) + " tries!")
             break;
         elif user_input == "exit":
             print("cya!")
             break;
         else:
             print("incorrect! guess again!")
+        tries+=1
 
 start(raw_input("Pick an upperbound of numbers to guess between! : 1 .. "))
