8c698625f7bdf8ec8d8014885c5893f98899b05f
github username <bteng1226@gmail.com>
2018-11-18 13:27:56 -0500

    added win percentages

--- guessing_game.py
+++ guessing_game.py
@@ -3,7 +3,7 @@ import time
 import sys
 
 def start(num):
-    while int(num) < 1:
+    while int(num) <= 1:
         num = raw_input("Please pick a number greater than 1: ")
     user_input = 0
     nums = range(int(num)+1)
@@ -17,14 +17,15 @@ def start(num):
         sys.stdout.flush()
 
     print "\nRandom number selected!"
+    print "You have a " + str(1/float(num)*100) +"% chance of getting it right on the first try!"
     sys.stdout.flush()
 
     random_num = str(random.randint(1,int(num)))
 
     while user_input != "exit" or user_input != random_num:
         user_input = raw_input("guess a number or type exit to end the game : ").strip()
+        tries+=1
         if user_input == random_num:
-            tries+=1
             print("you win! It took you " + str(tries) + " tries!")
             break;
         elif user_input == "exit":
@@ -32,6 +33,9 @@ def start(num):
             break;
         else:
             print("incorrect! guess again!")
-        tries+=1
+            if tries != 1:
+                print "You now have a " + str(tries/float(num)*100) +"% chance of getting it right!"
+
+
 
 start(raw_input("Pick an upperbound of numbers to guess between! : 1 .. "))
